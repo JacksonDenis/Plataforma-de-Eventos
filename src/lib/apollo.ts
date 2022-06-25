ea34ba3fb37fbeff8b5b960b1560426e9f504f30
Jackson Denis <jackson.denis@live.com>
2022-06-25 19:40:41 -0300

    Aula 04

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import {ApolloClient, InMemoryCache} from "@apollo/client";
 
 export const client = new ApolloClient ({
-    uri:'https://api-sa-east-1.graphcms.com/v2/cl4rdnf4s0yic01z6dc6efl0m/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN} `
+    },
     cache: new InMemoryCache()
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage : {
+        blur: "url(/src/assets/blur 2 2.png)"
+      },
       fontFamily: {
         sans: 'Roboto, sans-serif'
       },
